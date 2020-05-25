1a935214fdce596ce6f4cd61d41139e9736fea65
Karen Cabingatan <karen.cyantis@gmail.com>
2020-05-25 15:56:51 +0800

    Updates

---
+++ precache-manifest.407cc2826460cfb7bc6068a58d84c7a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03beb817d09e04276b3ad8fe8b534c8",
+    "url": "/Tinta-Preview/index.html"
+  },
+  {
+    "revision": "0787d5e6f434a497d7c3",
+    "url": "/Tinta-Preview/static/css/main.1ee194fc.chunk.css"
+  },
+  {
+    "revision": "8a85b1fe3aeaab8748e8",
+    "url": "/Tinta-Preview/static/js/2.0c12533d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tinta-Preview/static/js/2.0c12533d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0787d5e6f434a497d7c3",
+    "url": "/Tinta-Preview/static/js/main.eaac32c7.chunk.js"
+  },
+  {
+    "revision": "8078a1ed73e4159887a4",
+    "url": "/Tinta-Preview/static/js/runtime-main.20eba386.js"
+  },
+  {
+    "revision": "07d45cc385e7e523b540038e961d2742",
+    "url": "/Tinta-Preview/static/media/001.07d45cc3.jpg"
+  },
+  {
+    "revision": "ff35ebde22e46779b7f867837c69a4c8",
+    "url": "/Tinta-Preview/static/media/003.ff35ebde.jpg"
+  },
+  {
+    "revision": "7f4fc3db69b7366c2a267cc12ea35987",
+    "url": "/Tinta-Preview/static/media/004.7f4fc3db.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Tinta-Preview/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6677df6faa840c1992dfa0ca59744708",
+    "url": "/Tinta-Preview/static/media/rofitaste-webfont.6677df6f.woff2"
+  },
+  {
+    "revision": "f6c84d3063cae5bf005b17ce1bc10b4a",
+    "url": "/Tinta-Preview/static/media/rofitaste-webfont.f6c84d30.woff"
+  }
+]);
